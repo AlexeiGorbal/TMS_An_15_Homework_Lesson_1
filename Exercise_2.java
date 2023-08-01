8022cccc56974bc44d4ab9ba193d0c6340f6305a
AlexeiGorbal <141031450+AlexeiGorbal@users.noreply.github.com>
2023-08-01 19:18:09 +0300

    First homework

---
+++ Exercise_2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter user- ");
+        String user = scanner.nextLine();
+        System.out.println("Hello " + user);
+    }
+}
